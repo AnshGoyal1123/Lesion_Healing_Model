476a06d8fb5f02eb304e2da7021657b5ca2cfcfd
@AnshGoyal1123 <@agoyal2311@gmail.com>
2024-06-06 13:38:56 -0400

    another bm3d failure, testing without denoising

--- Healing_Model/Lesion_Healing.py
+++ Healing_Model/Lesion_Healing.py
@@ -19,38 +19,38 @@ model.to(device)
 model.eval()
 
 # Prepare your dataset
-#TODO: Update dataset size based on threshold........|
-dataset_directory = '/home/agoyal19/Dataset/Dataset_0.5/images'
+#TODO: Update dataset size based on threshold.......|
+dataset_directory = '/home/agoyal19/Dataset/Dataset_5/images'
 dataset = LesionedDataset(directory=dataset_directory)
 data_loader = DataLoader(dataset, batch_size=4, shuffle=False)
 
 # Directory to save the reconstructed images in NIfTI format
-#TODO: Update dataset size based on threshold.......................................|
-save_directory = '/home/agoyal19/Dataset/Reconstructions_BM3D/reconstructed_images_0.5'
+#TODO: Update dataset size based on threshold......................................|
+save_directory = '/home/agoyal19/Dataset/Reconstructions_BM3D/reconstructed_images_5'
 os.makedirs(save_directory, exist_ok=True)
 
-# Function to apply BM3D denoising
-def apply_bm3d_to_tensor(tensor, sigma_psd=30):
-    tensor_np = tensor.cpu().numpy()
-    print(f'Original tensor shape: {tensor_np.shape}')  # Debug statement
+# # Function to apply BM3D denoising
+# def apply_bm3d_to_tensor(tensor, sigma_psd=30):
+#     tensor_np = tensor.cpu().numpy()
+#     print(f'Original tensor shape: {tensor_np.shape}')  # Debug statement
 
-    # Ensure tensor is 3D (for CT images, it should be in (D, H, W) format)
-    if tensor_np.ndim == 4:
-        tensor_np = tensor_np[:, 0, :, :]  # Remove the channel dimension if present
-        print(f'Shape after removing channel dimension: {tensor_np.shape}')  # Debug statement
+#     # Ensure tensor is 3D (for CT images, it should be in (D, H, W) format)
+#     if tensor_np.ndim == 4:
+#         tensor_np = tensor_np[:, 0, :, :]  # Remove the channel dimension if present
+#         print(f'Shape after removing channel dimension: {tensor_np.shape}')  # Debug statement
 
-    # Ensure that each 3D volume is processed separately if batch size > 1
-    if tensor_np.ndim == 3:
-        denoised_tensor_np = bm3d.bm3d(tensor_np, sigma_psd=sigma_psd)
-        denoised_tensor_np = np.expand_dims(denoised_tensor_np, axis=0)  # Add batch dimension
-    else:
-        denoised_tensor_np = np.array([bm3d.bm3d(tensor_np[i], sigma_psd=sigma_psd) for i in range(tensor_np.shape[0])])
+#     # Ensure that each 3D volume is processed separately if batch size > 1
+#     if tensor_np.ndim == 3:
+#         denoised_tensor_np = bm3d.bm3d(tensor_np, sigma_psd=sigma_psd)
+#         denoised_tensor_np = np.expand_dims(denoised_tensor_np, axis=0)  # Add batch dimension
+#     else:
+#         denoised_tensor_np = np.array([bm3d.bm3d(tensor_np[i], sigma_psd=sigma_psd) for i in range(tensor_np.shape[0])])
 
-    # Add back the channel dimension
-    denoised_tensor_np = np.expand_dims(denoised_tensor_np, axis=1)
-    print(f'Shape after BM3D denoising: {denoised_tensor_np.shape}')  # Debug statement
+#     # Add back the channel dimension
+#     denoised_tensor_np = np.expand_dims(denoised_tensor_np, axis=1)
+#     print(f'Shape after BM3D denoising: {denoised_tensor_np.shape}')  # Debug statement
 
-    return torch.from_numpy(denoised_tensor_np).to(tensor.device)
+#     return torch.from_numpy(denoised_tensor_np).to(tensor.device)
 
 # Validation and saving reconstructed images
 total_loss = 0
@@ -65,19 +65,19 @@ with torch.no_grad():
         print(f'Reconstructed images shape: {reconstructed_images.shape}')  # Debug statement
 
         # Denoise using BM3D
-        reconstructed_images_denoised = apply_bm3d_to_tensor(reconstructed_images)
+        # reconstructed_images_denoised = apply_bm3d_to_tensor(reconstructed_images)
 
         # Calculate the reconstruction loss (MSE)
-        reconstruction_loss = F.mse_loss(reconstructed_images_denoised, images)
+        reconstruction_loss = F.mse_loss(reconstructed_images, images)
         loss = reconstruction_loss + quantization_loss
         total_loss += loss.item()
 
         # Save reconstructed and denoised images as .nii files
         for j in range(images.size(0)):
-            img_data = reconstructed_images_denoised[j, 0].cpu().numpy()
+            img_data = reconstructed_images[j, 0].cpu().numpy()
             img_nii = nib.Nifti1Image(img_data, affine=np.eye(4))  # Assuming no need for specific affine
             original_name = filenames[j].replace('.nii.gz', '')  # Remove .nii.gz extension for new filename
-            img_path = os.path.join(save_directory, f'{original_name}_reconstructed_denoised.nii.gz')
+            img_path = os.path.join(save_directory, f'{original_name}_reconstructed.nii.gz')
             nib.save(img_nii, img_path)
 
 # Compute average loss
