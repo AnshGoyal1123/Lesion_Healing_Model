0a2e7a8aafdc4fe27088d3427d2a08692ba7e2bf
@AnshGoyal1123 <@agoyal2311@gmail.com>
2024-07-29 16:24:43 -0400

    edited model

--- Healing_Model/VAE-GAN/Healthy_Training_GAN.py
+++ Healing_Model/VAE-GAN/Healthy_Training_GAN.py
@@ -1,20 +1,26 @@
 import torch
 from HealthyData import HealthyDataset
-from VAE_GAN_Implementation import VAEGAN, train_vaegan, save_model
+from VAE_GAN_Implementation import VQVAEGAN, train_vqvaegan, save_model
 
 # Dataset Directory
 dataset_directory = '/home/agoyal19/Dataset/control_data'  # Update this to your directory
 dataset = HealthyDataset(directory=dataset_directory)
 
 # Model Initialization
-input_channels = 1  # Assuming grayscale brain images
+input_channels = 1  # Assuming single-channel (grayscale) 3D brain images
 hidden_channels = 32
-z_dim = 64
-model = VAEGAN(input_channels, hidden_channels, z_dim)
+embedding_dim = 64
+num_embeddings = 512  # Number of discrete embeddings
+commitment_cost = 0.25  # Weisght of the commitment loss
+
+model = VQVAEGAN(input_channels, hidden_channels, embedding_dim, num_embeddings, commitment_cost)
 
 # Train the model
-trained_model = train_vaegan(model, dataset, num_epochs=500, batch_size=4, lr=1e-4)
+num_epochs = 500
+batch_size = 4
+learning_rate = 1e-4
+trained_model = train_vqvaegan(model, dataset, num_epochs=num_epochs, batch_size=batch_size, lr=learning_rate)
 
 # Save the trained model
-model_save_path = "/home/agoyal19/My_Work/Healing_Model/Healthy_Models/healthy_vaegan.pth"
+model_save_path = "/home/agoyal19/My_Work/Healing_Model/Healthy_Models/healthy_vqvaegan.pth"
 save_model(trained_model, model_save_path)
--- Healing_Model/VAE-GAN/VAE_GAN_Implementation.py
+++ Healing_Model/VAE-GAN/VAE_GAN_Implementation.py
@@ -3,28 +3,67 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
 
+# Vector Quantizer Module
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_cost):
+        super(VectorQuantizer, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.num_embeddings = num_embeddings
+        self.commitment_cost = commitment_cost
+
+        self.embedding = nn.Embedding(self.num_embeddings, self.embedding_dim)
+        self.embedding.weight.data.uniform_(-1.0 / self.num_embeddings, 1.0 / self.num_embeddings)
+
+    def forward(self, x):
+        # Flatten input
+        flat_input = x.view(-1, self.embedding_dim)
+
+        # Calculate distances
+        distances = (torch.sum(flat_input**2, dim=1, keepdim=True) 
+                     + torch.sum(self.embedding.weight**2, dim=1)
+                     - 2 * torch.matmul(flat_input, self.embedding.weight.t()))
+
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self.num_embeddings, device=x.device)
+        encodings.scatter_(1, encoding_indices, 1)
+
+        # Quantize
+        quantized = torch.matmul(encodings, self.embedding.weight).view(x.shape)
+
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), x)
+        q_latent_loss = F.mse_loss(quantized, x.detach())
+        loss = q_latent_loss + self.commitment_cost * e_latent_loss
+
+        quantized = x + (quantized - x).detach()
+
+        avg_probs = torch.mean(encodings, dim=0)
+        perplexity = torch.exp(-torch.sum(avg_probs * torch.log(avg_probs + 1e-10)))
+
+        return quantized, loss, perplexity
+
 # Encoder
 class Encoder(nn.Module):
-    def __init__(self, input_channels, hidden_channels, z_dim):
+    def __init__(self, input_channels, hidden_channels, embedding_dim):
         super(Encoder, self).__init__()
         self.conv1 = nn.Conv3d(input_channels, hidden_channels, kernel_size=4, stride=2, padding=1)
         self.conv2 = nn.Conv3d(hidden_channels, hidden_channels, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv3d(hidden_channels, hidden_channels, kernel_size=3, stride=1, padding=1)
-        self.conv4 = nn.Conv3d(hidden_channels, z_dim*2, kernel_size=3, stride=1, padding=1)  # z_dim*2 for mean and logvar
+        self.conv4 = nn.Conv3d(hidden_channels, embedding_dim, kernel_size=3, stride=1, padding=1)
 
     def forward(self, x):
         x = F.relu(self.conv1(x))
         x = F.relu(self.conv2(x))
         x = F.relu(self.conv3(x))
         x = self.conv4(x)
-        mean, logvar = torch.chunk(x, 2, dim=1)  # Split into mean and logvar
-        return mean, logvar
+        return x
 
 # Decoder
 class Decoder(nn.Module):
-    def __init__(self, output_channels, hidden_channels, z_dim):
+    def __init__(self, output_channels, hidden_channels, embedding_dim):
         super(Decoder, self).__init__()
-        self.conv1 = nn.ConvTranspose3d(z_dim, hidden_channels, kernel_size=3, stride=1, padding=1)
+        self.conv1 = nn.ConvTranspose3d(embedding_dim, hidden_channels, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.ConvTranspose3d(hidden_channels, hidden_channels, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.ConvTranspose3d(hidden_channels, hidden_channels, kernel_size=3, stride=1, padding=1)
         self.conv4 = nn.ConvTranspose3d(hidden_channels, output_channels, kernel_size=4, stride=2, padding=1, output_padding=1)
@@ -50,32 +89,27 @@ class Discriminator(nn.Module):
         x = F.leaky_relu(self.conv2(x), 0.2)
         x = F.leaky_relu(self.conv3(x), 0.2)
         x = self.conv4(x)
-        return torch.sigmoid(x)  # Use sigmoid to constrain output to [0, 1]
+        return torch.sigmoid(x)
 
-# VAE-GAN Model
-class VAEGAN(nn.Module):
-    def __init__(self, input_channels, hidden_channels, z_dim):
-        super(VAEGAN, self).__init__()
-        self.encoder = Encoder(input_channels, hidden_channels, z_dim)
-        self.decoder = Decoder(input_channels, hidden_channels, z_dim)
+# VQ-VAE-GAN Model
+class VQVAEGAN(nn.Module):
+    def __init__(self, input_channels, hidden_channels, embedding_dim, num_embeddings, commitment_cost):
+        super(VQVAEGAN, self).__init__()
+        self.encoder = Encoder(input_channels, hidden_channels, embedding_dim)
+        self.quantizer = VectorQuantizer(num_embeddings, embedding_dim, commitment_cost)
+        self.decoder = Decoder(input_channels, hidden_channels, embedding_dim)
         self.discriminator = Discriminator(input_channels, hidden_channels)
 
-    def reparameterize(self, mean, logvar):
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn_like(std)
-        return mean + eps * std
-
     def forward(self, x):
-        mean, logvar = self.encoder(x)
-        z = self.reparameterize(mean, logvar)
-        x_recon = self.decoder(z)
-        return x_recon, mean, logvar
+        z = self.encoder(x)
+        quantized, quantization_loss, perplexity = self.quantizer(z)
+        x_recon = self.decoder(quantized)
+        return x_recon, quantization_loss, perplexity
 
 # Loss Functions
-def vae_loss(x, x_recon, mean, logvar):
+def vae_loss(x, x_recon, quantization_loss):
     recon_loss = F.mse_loss(x_recon, x, reduction='sum')
-    kld_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
-    return recon_loss + kld_loss
+    return recon_loss + quantization_loss
 
 def gan_loss(real_output, fake_output):
     real_loss = F.binary_cross_entropy(real_output, torch.ones_like(real_output))
@@ -83,27 +117,27 @@ def gan_loss(real_output, fake_output):
     return real_loss + fake_loss
 
 # Training function
-def train_vaegan(model, dataset, num_epochs=100, batch_size=4, lr=1e-4, device=None):
+def train_vqvaegan(model, dataset, num_epochs=100, batch_size=4, lr=1e-4, device=None):
     if device is None:
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
     
     data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
-    vae_optimizer = torch.optim.Adam(list(model.encoder.parameters()) + list(model.decoder.parameters()), lr=lr)
+    vae_optimizer = torch.optim.Adam(list(model.encoder.parameters()) + list(model.decoder.parameters()) + list(model.quantizer.parameters()), lr=lr)
     discriminator_optimizer = torch.optim.Adam(model.discriminator.parameters(), lr=lr)
 
     for epoch in range(num_epochs):
         epoch_vae_loss = 0
         epoch_gan_loss = 0
         for batch_data in data_loader:
-            images = batch_data['ct'].to(device).float()  # Ensure your dataset outputs the correct key
+            images = batch_data['ct'].to(device).float()
 
-            # VAE-GAN forward pass
-            x_recon, mean, logvar = model(images)
+            # VQ-VAE-GAN forward pass
+            x_recon, quantization_loss, _ = model(images)
             
             # VAE loss
-            recon_loss = vae_loss(images, x_recon, mean, logvar)
+            recon_loss = vae_loss(images, x_recon, quantization_loss)
             
             # Discriminator forward pass
             real_output = model.discriminator(images)
@@ -141,4 +175,3 @@ def load_model(model, path, device=None):
     model.load_state_dict(torch.load(path, map_location=device))
     model.to(device)
     print(f"Model loaded from {path}")
-   
